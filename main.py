d220299c8039cbb97cc2d84571d1cdea82a4db86
kartik <singhkartik45057@gmail.com>
2026-02-10 19:00:56 +0530

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from Rag.Workflow import Execute_Workflow
+
+
+#-----------------------------------------------------------------------------------------------
+result = Execute_Workflow("explain numpy")
+print("result ", result["page_content"])
+
+
+
